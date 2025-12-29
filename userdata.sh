29fb7d0e3c84e9bd896cfece798b15afe0ccd422
sravan-kumarc <sravankumarchagalamarri@gmail.com>
2025-12-29 13:13:26 +0000

    16

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 sudo yum update -y
+
+# Install Node.js 20 (LTS)
+curl -fsSL https://rpm.nodesource.com/setup_20.x | bash -
+yum install -y nodejs
+
+# Verify
+node -v
+npm -v
+
+
 sudo yum install ruby -y
 sudo yum install wget -y
 sudo yum install curl -y
